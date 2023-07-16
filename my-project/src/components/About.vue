aac6f608b2b09f430efd5a5ea212e1ad5b355e26
Nikita0x <klirmio21@gmail.com>
2023-07-16 22:34:06 +0300

    testing the work of env sensitive variables

--- my-project/src/components/About.vue
+++ my-project/src/components/About.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class=" text-green-600">
+    <div class="text-green-600 ">
         about
     </div>
+    <h1>{{ apiKey }} - api key</h1>
 </template>
 
 <script setup>
-
+const apiKey = import.meta.env.VITE_API_KEY
+console.log(apiKey)
 </script>
