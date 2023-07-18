5ac5d8ddb318ff7918cbf740be0517d42666a392
Nikita0x <klirmio21@gmail.com>
2023-07-18 17:49:37 +0300

    finally managed to set up first openWeather api component logic

--- my-project/src/components/components/LogicComponents/OpenWeatherAPI.vue
+++ my-project/src/components/components/LogicComponents/OpenWeatherAPI.vue
@@ -10,8 +10,12 @@
       <h1>hi</h1>
       <p>Output: {{ cityName }}</p>
       <p>Output on enter: {{ inputOnEnter }}</p>
-      <button  class="bg-green-600 border">Test button to test global store:</button>
-      <p>{{ store }}</p>
+      <button @click="console.log(weatherData)"  class="bg-green-600 border">Test button to test global store:</button>
+      <p>{{ store.getters }}</p>
+      <p v-if="country">Country: {{ country }}</p>
+      <p v-if="lat">lat: {{ lat }}</p>
+      <p v-if="lon">lon: {{ lon }}</p>
+      <p v-if="city">city: {{ city }}</p>
     </div>
   </template>
   
@@ -20,6 +24,7 @@
   import { ref, computed } from "vue";
   import { useStore } from "vuex" // Import the useStore function
   const store = useStore(); // Access the store using the useStore function
+
 
   const API_KEY = import.meta.env.VITE_OPENWEATHER_API
   
@@ -33,6 +38,22 @@
     return cityName.value.toLowerCase();
   });
   
+  // parsing weatherData from global store
+  const weatherData = computed(() => {
+    return store.state.weatherData
+  })
+  const country = computed(() => {
+    return store.getters.getCountry
+  })
+  const lat = computed(() => {
+    return store.getters.getLat
+  })
+  const lon = computed(() => {
+    return store.getters.getLon
+  })
+  const city = computed(() => {
+    return store.getters.getCity
+  })
 
 //async weather data retrieval on enter 
 async function fetchWeather() {
@@ -40,10 +61,9 @@ async function fetchWeather() {
     
     try {
       const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName.value}&limit=${limit}&appid=${API_KEY}`
-    )
-    const data = await response.json();
-    store.commit("setWeatherData", data)
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName.value}&limit=${limit}&appid=${API_KEY}`)
+      const data = await response.json();
+      store.commit("setWeatherData", data[0])
     } catch (error) {
       console.error(error)
     }
--- my-project/src/store/store.js
+++ my-project/src/store/store.js
@@ -4,17 +4,42 @@ import { createStore } from 'vuex';
 const store = createStore({
   state() {
     return {
+      //here we define the name for the global store
       weatherData: null,
     };
   },
   mutations: {
+    //here we define mutations, methods with which the data will be stored in the global store
     setWeatherData(state, data) {
       state.weatherData = data;
     },
   },
   getters: {
+    //here we define different getters, to retrieve needed information from the global data, to be able to access it and use it in our components.
+    //We need to always check first if the data is present, because if it is not present - it will result in an error
     getCountry (state) {
-      return state.weatherData
+      if(state.weatherData) {
+        return state.weatherData.country
+      }
+      return null
+    },
+    getLat (state) {
+      if(state.weatherData) {
+        return state.weatherData.lat
+      }
+      return null
+    },
+    getLon (state) {
+      if(state.weatherData) {
+        return state.weatherData.lon
+      }
+      return null
+    },
+    getCity (state) {
+      if(state.weatherData) {
+        return state.weatherData.name
+      }
+      return null
     }
   }
 });
