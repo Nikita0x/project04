e57ab47f31f83257802825a472a6c57725cde383
Nikita0x <klirmio21@gmail.com>
2023-07-25 16:54:07 +0300

    fixed submit button

--- my-project/src/components/views/Feedback.vue
+++ my-project/src/components/views/Feedback.vue
@@ -54,7 +54,7 @@
       </div>
 
       <div class="flex justify-center">
-        <button type="button" class="text-white bg-[#050708] hover:bg-[#050708]/80 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 dark:focus:ring-gray-600 mr-2 mb-2 transition">Submit</button>
+        <button type="submit" class="text-white bg-[#050708] hover:bg-[#050708]/80 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 dark:focus:ring-gray-600 mr-2 mb-2 transition">Submit</button>
       </div>
     </form>
   </div>
